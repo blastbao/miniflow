4d197d6c6bf00a6695683ca0d4e25971f754c534
emryx <emryx26@gmail.com>
2021-01-27 22:54:24 +0800

    update topo_test

--- ds/topo_test.go
+++ ds/topo_test.go
@@ -9,22 +9,18 @@ func TestTopoOrder(t *testing.T) {
 	to := []int{1, 6, 5, 4, 9, 4, 5, 0, 3, 7, 6, 10, 11, 12, 12}
 	edgeSize := len(fm)
 
-	g := NewDigraph(13)
+	g := NewDigraph()
 	for i := 0; i < edgeSize; i++ {
 		g.AddEdge(fm[i], to[i])
 	}
 
 	topoIn := NewTopo(g, g.Parents, 5)
-	t.Log("topo indegree order")
-	t.Log("topo: ", topoIn.GetPostOrder())
+	t.Log("indegree order")
+	t.Log("post: ", topoIn.GetPostOrder())
 	t.Log("topo: ", topoIn.GetTopoOrder())
 
 	topoOut := NewTopo(g, g.Children, 5)
-	t.Log("topo outdegree order")
-	t.Log("topo: ", topoOut.GetPostOrder())
+	t.Log("outdegree order")
+	t.Log("post: ", topoOut.GetPostOrder())
 	t.Log("topo: ", topoOut.GetTopoOrder())
-	//t.Log("reverse graph")
-	//rg := g.Reverse()
-	//dfo1 := NewTopo(rg, 5)
-	//t.Log("topo: ", dfo1.GetTopoOrder())
 }
