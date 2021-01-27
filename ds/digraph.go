c17ae8991931c9a5ea87af3ed2a0be5a8f368223
emryx <emryx26@gmail.com>
2021-01-27 08:13:08 +0800

    update digraph

--- ds/digraph.go
+++ ds/digraph.go
@@ -7,120 +7,166 @@ import (
 
 // Digraph provides some directed graph api
 type Digraph interface {
-	GetVSize() int
-	GetV() []int
-	GetAdj(v int) []int
-	GetAdjSize(v int) int
+	Size() int
+	V() []int
+	Parents(v int) []int
+	Children(v int) []int
+	Indegree(v int) int
+	Outdegree(v int) int
 	AddV(v int)
 	AddEdge(v, w int)
 	DelV(v int) error
 	DelEdge(v, w int) error
-	Reverse() Digraph
+	Index(v int) int
 }
 
-type adj map[int]*set
+type adj map[int]Vertex
 
 type digraph struct {
 	size     int
 	vertices []int
-	adj      *adj
-	seen     map[int]int
+	adj      adj
 }
 
 // NewDigraph create a new directed graph
-func NewDigraph() Digraph {
-	adj := make(adj)
-	seen := make(map[int]int)
-	return &digraph{size: 0, adj: &adj, seen: seen}
+func NewDigraph(size int) Digraph {
+	return &digraph{
+		size:     0,
+		vertices: make([]int, size),
+		adj:      make(adj, size),
+	}
+}
+
+func (g *digraph) Index(v int) int {
+	if vtx, err := g.getAdj(v); err == nil {
+		return vtx.Index()
+	}
+	return -1
 }
 
 // GetVSize return number of vertices
-func (g *digraph) GetVSize() int { return g.size }
+func (g *digraph) Size() int { return g.size }
 
 // GetV return vertices of graph
-func (g *digraph) GetV() []int { return g.vertices }
+func (g *digraph) V() []int { return g.vertices }
 
-// GetAdj return children of v
-func (g *digraph) GetAdj(v int) []int {
-	set, _ := g.getAdj(v)
-	return set.vertices
+// Parents return parents of v
+func (g *digraph) Parents(v int) []int {
+	if vtx, err := g.getAdj(v); err == nil {
+		return vtx.Parents()
+	}
+	return nil
 }
 
-// GetAdjSize return children size of v
-func (g *digraph) GetAdjSize(v int) int {
-	set, _ := g.getAdj(v)
-	return set.size
+// GetAdj return children of v
+func (g *digraph) Children(v int) []int {
+	if vtx, err := g.getAdj(v); err == nil {
+		return vtx.Children()
+	}
+	return nil
+}
+
+func (g *digraph) Indegree(v int) int {
+	if vtx, err := g.getAdj(v); err == nil {
+		return vtx.Indegree()
+	}
+	return 0
+}
+
+// GetAdjSize return outdegree of v
+func (g *digraph) Outdegree(v int) int {
+	if vtx, err := g.getAdj(v); err == nil {
+		return vtx.Outdegree()
+	}
+	return 0
 }
 
 //AddV add vertex to graph
 func (g *digraph) AddV(v int) { g.addV(v) }
 
-// AddEdge add directed edge to graph
-func (g *digraph) AddEdge(from int, to int) {
-	g.addV(from)
-	g.addV(to)
-	set, _ := g.getAdj(from)
-	set.add(to)
+// AddEdge add directed edge (v -> w)
+func (g *digraph) AddEdge(v, w int) {
+	g.addV(v)
+	g.addV(w)
+	from, _ := g.getAdj(v)
+	from.AddChild(w)
+	to, _ := g.getAdj(w)
+	to.AddParent(v)
 }
 
 // DelV delete v from graph
 func (g *digraph) DelV(v int) error {
-	if _, exists := (*g.adj)[v]; !exists {
-		err := fmt.Sprintf("vertex not exists: %d\n", v)
-		return errors.New(err)
+	vtx, err := g.getAdj(v)
+	if err != nil {
+		return err
 	}
-	g.size--
-	i := g.seen[v]
-	last := g.vertices[g.size]
-	g.seen[last] = i
-	g.vertices[i] = g.vertices[g.size]
-	g.vertices[g.size] = 0
-	g.vertices = g.vertices[:g.size]
-	delete(*g.adj, v)
-	delete(g.seen, v)
+	index := vtx.Index()
+	g.delVertex(index)
+	for _, child := range vtx.Children() {
+		if err = g.delParent(v, child); err != nil {
+			return err
+		}
+	}
+	for _, parent := range vtx.Parents() {
+		if err = g.delChild(parent, v); err != nil {
+			return err
+		}
+	}
+	delete(g.adj, v)
 	return nil
 }
 
 // DelEdge delete edge (v -> w)
 func (g *digraph) DelEdge(v, w int) error {
-	set, err := g.getAdj(v)
-	if err != nil {
+	if err := g.delParent(v, w); err != nil {
 		return err
 	}
-	set.delete(w)
+	if err := g.delChild(v, w); err != nil {
+		return err
+	}
 	return nil
 }
 
-// Reverse makes a reverse graph
-func (g *digraph) Reverse() Digraph {
-	rg := NewDigraph()
-	for from := range *g.adj {
-		rg.AddV(from)
-		set, _ := g.getAdj(from)
-		for _, to := range set.vertices {
-			rg.AddEdge(to, from)
-		}
+func (g *digraph) delVertex(index int) {
+	g.size--
+	g.vertices[index] = g.vertices[g.size]
+	g.vertices[g.size] = 0
+	g.vertices = g.vertices[:g.size]
+}
+
+func (g *digraph) delParent(from, to int) error {
+	child, err := g.getAdj(to)
+	if err != nil {
+		return err
 	}
-	return rg
+	child.DelParent(from)
+	return nil
+}
+
+func (g *digraph) delChild(from, to int) error {
+	parent, err := g.getAdj(from)
+	if err != nil {
+		return err
+	}
+	parent.DelChild(to)
+	return nil
 }
 
 func (g *digraph) addV(v int) {
-	if _, exists := (*g.adj)[v]; exists {
+	if _, exists := g.adj[v]; exists {
 		return
 	}
-	(*g.adj)[v] = newSet()
+	g.adj[v] = newVertex(g.size)
 	g.vertices = append(g.vertices, v)
-	g.seen[v] = g.size
 	g.size++
 }
 
-func (g *digraph) getAdj(v int) (*set, error) {
-	if _, exists := (*g.adj)[v]; !exists {
-		err := fmt.Sprintf("adj not exists: %d\n", v)
+func (g *digraph) getAdj(v int) (Vertex, error) {
+	if _, exists := g.adj[v]; !exists {
+		err := fmt.Sprintf("vertex not exists: %d\n", v)
 		return nil, errors.New(err)
 	}
-	return (*g.adj)[v], nil
+	return g.adj[v], nil
 }
 
-func (s set) String() string     { return fmt.Sprintf("%v", s.vertices) }
 func (g digraph) String() string { return fmt.Sprintf("%v", g.adj) }
--- ds/digraph_test.go
+++ ds/digraph_test.go
@@ -7,38 +7,99 @@ import (
 func TestDigraph(t *testing.T) {
 	fm := []int{0, 0, 0, 6, 6, 5, 3, 2, 2, 8, 7, 9, 9, 9, 11}
 	to := []int{1, 6, 5, 4, 9, 4, 5, 0, 3, 7, 6, 10, 11, 12, 12}
-	edgeSize := len(fm)
+	size := 13
 
-	g := NewDigraph()
-	want, actual := 0, g.GetVSize()
+	g := NewDigraph(size)
+	want, actual := 0, g.Size()
 	if want != actual {
 		t.Fatalf("want: %d, actual: %d\n", want, actual)
 	}
-	for i := 0; i < edgeSize; i++ {
+	for i := 0; i < 15; i++ {
 		g.AddEdge(fm[i], to[i])
 	}
 
-	want, actual = 13, g.GetVSize()
-	if want != actual {
-		t.Fatalf("want: %d, actual: %d\n", want, actual)
+	tests := []struct {
+		want   int
+		vertex int
+	}{
+		{3, 0},
+		{0, 1},
+		{2, 2},
+		{1, 3},
+		{0, 4},
+		{1, 5},
+		{2, 6},
+		{1, 7},
+		{1, 8},
+		{3, 9},
+		{0, 10},
+		{1, 11},
+		{0, 12},
 	}
 
-	rg := g.Reverse()
-	want, actual = 13, rg.GetVSize()
-	if want != actual {
-		t.Fatalf("want: %d, actual: %d\n", want, actual)
+	for _, c := range tests {
+		actual = g.Outdegree(c.vertex)
+		if c.want != actual {
+			t.Errorf("want: %d, actual %d", c.want, actual)
+		}
+	}
+
+	tests = []struct {
+		want   int
+		vertex int
+	}{
+		{1, 0},
+		{1, 1},
+		{0, 2},
+		{1, 3},
+		{2, 4},
+		{2, 5},
+		{2, 6},
+		{1, 7},
+		{0, 8},
+		{1, 9},
+		{1, 10},
+		{1, 11},
+		{2, 12},
+	}
+
+	for _, c := range tests {
+		actual = g.Indegree(c.vertex)
+		if c.want != actual {
+			t.Errorf("want: %d, actual %d", c.want, actual)
+		}
+	}
+
+	tests = []struct {
+		want   int
+		vertex int
+	}{
+		{0, 0},
+		{1, 1},
+		{7, 2},
+		{6, 3},
+		{4, 4},
+		{3, 5},
+		{2, 6},
+		{9, 7},
+		{8, 8},
+		{5, 9},
+		{10, 10},
+		{11, 11},
+		{12, 12},
+	}
+
+	for _, c := range tests {
+		actual = g.Index(c.vertex)
+		if c.want != actual {
+			t.Errorf("want: %d, actual %d", c.want, actual)
+		}
 	}
 
 	// delete
 	g.DelEdge(0, 1)
 	g.DelV(0)
-	want, actual = 12, g.GetVSize()
-	if want != actual {
-		t.Fatalf("want: %d, actual: %d\n", want, actual)
-	}
-
-	rg = g.Reverse()
-	want, actual = 13, rg.GetVSize()
+	want, actual = 12, g.Size()
 	if want != actual {
 		t.Fatalf("want: %d, actual: %d\n", want, actual)
 	}
